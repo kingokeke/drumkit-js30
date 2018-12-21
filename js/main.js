d4caa3c71010f95a0d62dddea3b9e522f2be7940
Kingsley Okeke <kingsleyokeke.ng@gmail.com>
2018-12-21 20:45:20 +0100

    Added JS functionality for playing sounds

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const playSound = e => {
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+	if (!audio) return;
+	audio.currentTime = 0;
+	audio.play();
+};
+
+window.addEventListener('keydown', e => {
+	playSound(e);
+});
