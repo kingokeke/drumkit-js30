f64531252c289c38ef8f2433ccacd78e2e096958
Kingsley Okeke <kingsleyokeke.ng@gmail.com>
2018-12-21 21:27:59 +0100

    Added functionality for animating page on keydown

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+'use strict';
+
+// Function to play the sounds from the drumkit when key is pressed
 const playSound = e => {
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 	if (!audio) return;
@@ -5,6 +8,18 @@ const playSound = e => {
 	audio.play();
 };
 
+// Function to animate the page when key is pressed
+const animatePage = e => {
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+	if (!key) return;
+	key.classList.add('playing');
+	window.addEventListener('transitionend', () => {
+		key.classList.remove('playing');
+	});
+};
+
+// Add event listener to trigger playSound and animatePage on keydown
 window.addEventListener('keydown', e => {
 	playSound(e);
+	animatePage(e);
 });
